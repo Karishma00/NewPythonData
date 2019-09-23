11fc36591d1719ef6dc19c4865d4c2b86cf66e1a
Karishma00 <karishma11198@gmail.com>
2019-09-23 15:52:03 +0530

    forth commit

--- OddEvenCapEx19.py
+++ OddEvenCapEx19.py
@@ -2,13 +2,18 @@
 
 def myfun(s):
     x=[]
+    new = ""
     for i,a in enumerate(s):
         if i%2==0:
             a=a.upper()
             x.append(a)
         else:
             x.append(a)
-    return x
+
+    for d in x:
+        new += d
+
+    return new
 
 print(myfun('abcde'))
 
