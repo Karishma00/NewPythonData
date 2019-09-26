a8a796e93c9ac1f6688894d9ffeefff7044be338
Karishma00 <karishma11198@gmail.com>
2019-09-26 10:06:16 +0530

    Advanced Strings

---
+++ AdvancedObjectDataStructure/Strings.py
@@ -0,0 +1,24 @@
+#Strings
+
+str1="Advanced Strings"
+
+
+print('Capitalize : ',str1.capitalize())
+print('Title :',str1.title())
+print('Count :',str1.count('a'))
+print('Find : ',str1.find('s'))
+print('Center :',str1.center(20,'.'))#Places the string between . and length is 20
+
+s2='Expanding\tStrings'
+print('Expand Tabs :',s2.expandtabs())
+
+#Alphanumeric
+n='12333'
+print(f'{n} is : alphanumeric {n.isalnum()}')
+
+#Alphabatic
+print(f'{n} is : alphbatic {n.isalpha()}')
+
+#endswith
+a='Abcde'
+print(f'{a} is end with e : {a.endswith("e")}')
